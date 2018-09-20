ca039d8c09296f51c97e99cdee19facd889f7c85
beatrisia <nvelentsey@gmail.com>
2018-09-20 17:06:15 +0300

    переделка задачи

--- kvadrat.rb
+++ kvadrat.rb
@@ -1,24 +1,22 @@
 puts "а:"
-a = gets.chomp.to_f
+a = gets.to_f
 
 puts "b:"
-b = gets.chomp.to_f
+b = gets.to_f
 
 puts "c:"
-c = gets.chomp.to_f
+c = gets.to_f
 
-D = b**2 - 4 * a * c
+d = b**2 - 4 * a * c
+square_root = Math.sqrt(d)
 
-if D < 0
-	puts "D = #{D}, корешков нет."
-
-elsif D > 0
-x1 = (-b + Math.sqrt(D)) / (2 * a)
-x2 = (-b - Math.sqrt(D)) / (2 * a)
-	puts "D = #{D}, x1 = #{x1}, x2 = #{x2}"
-
-elsif D = 0
-x1 = (-b + Math.sqrt(D)) / (2 * a)
-	puts "D = #{D}, x1 = x2 = #{x1}"
-
+if d < 0
+  puts "D = #{d}, корешков нет."
+elsif d > 0
+  x1 = (-b + #{square_root}) / (2 * a)
+  x2 = (-b - #{square_root}) / (2 * a)
+  puts "D = #{d}, x1 = #{x1}, x2 = #{x2}"
+else
+  x1 = -b / (2 * a)
+  puts "D = #{d}, x1 = x2 = #{x1}"
 end
